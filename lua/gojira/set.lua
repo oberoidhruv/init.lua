db0575deb28bd9307648df056bb2a96deed12eaa
OberoiDhruv <dhruv.oberoi@gmail.com>
2023-08-11 09:26:33 +1000

    updated set

--- lua/gojira/set.lua
+++ lua/gojira/set.lua
@@ -28,3 +28,19 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+vim.opt.mouse = 'r'
+
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.g.copilot_filetypes = {
+    ["*"] = false,
+    ["javascript"] = true,
+    ["typescript"] = true,
+    ["lua"] = false,
+    ["rust"] = true,
+    ["c"] = true,
+    ["c#"] = true,
+    ["c++"] = true,
+    ["go"] = true,
+    ["python"] = true,
+}
